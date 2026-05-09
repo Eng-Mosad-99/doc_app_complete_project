54bd12d0c0215b30e0352696613b85c36ccae4cf
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-05-09 10:54:31 -0400

    create routing folder that contains routes class and app router class

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,17 @@
+import 'package:doc_app_complete_project/core/routing/routes.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const Placeholder());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(child: Text('No route defined for ${settings.name}')),
+          ),
+        );
+    }
+  }
+}
---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static const String onBoardingScreen = '/onBoarding';
+  static const String loginScreen = '/login';
+  static const String registerScreen = '/register';
+  static const String homeScreen = '/home';
+}
