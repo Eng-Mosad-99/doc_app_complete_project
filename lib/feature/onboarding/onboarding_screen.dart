b5beae0f83bae399000f009c9d1969b4f0beedbf
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-05-09 11:17:57 -0400

    create DocLogoAndName widget

--- lib/feature/onboarding/onboarding_screen.dart
+++ lib/feature/onboarding/onboarding_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'widgets/doc_logo_and_name.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(top: 30.h, bottom: 30.h),
+            child: Column(children: <Widget>[const DocLogoAndName()]),
+          ),
+        ),
+      ),
+    );
   }
 }
